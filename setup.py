2c0d520934991253a9cf707f9f604feb452119b9
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-06-25 21:29:13 +1000

    Increment version to 1.3.0.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ classifiers = [
 
 setup_kwargs = dict(
     name='powershift-cli',
-    version='1.2.7',
+    version='1.3.0',
     description='Pluggable command line client for OpenShift.',
     long_description=long_description,
     url='https://github.com/getwarped/powershift-cli',
